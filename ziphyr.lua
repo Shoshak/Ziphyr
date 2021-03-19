381c283752698235e956525e154f2ad5b8b7e27d
KoshakLoL <61428316+KoshakLoL@users.noreply.github.com>
2021-03-19 21:46:34 +0500

    Update ziphyr.lua

--- ziphyr.lua
+++ ziphyr.lua
@@ -14,8 +14,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see https://www.gnu.org/licenses
 
-
-
 local filesystem = require("filesystem")
 local shell = require("shell")
 local computer = require("computer")
